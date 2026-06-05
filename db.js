8d328f847549491755c81cda40a394f08c629e43
DiasAbraao1 <diasabraao08@gmail.com>
2026-06-05 14:59:11 -0300

    adicionando arquivo db

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Abrdi6#2007', 
+    password: '', 
     database: 'healthtrack_db'
 });
 
